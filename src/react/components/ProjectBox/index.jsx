b4d97daf1af83659f138f21360ba75d2ed775251
Casca0 <lucashborzani@gmail.com>
2023-01-17 03:54:52 -0300

    Mudança na lógica de imagem da caixa de projetos

--- src/react/components/ProjectBox/index.jsx
+++ src/react/components/ProjectBox/index.jsx
@@ -3,11 +3,14 @@
 import React from 'react';
 import './style.scss';
 
+import DiscordIcon from '@assets/discord-icon-svgrepo-com.webp';
+import ProgrammingIcon from '@assets/programming-code-svgrepo-com.webp';
+
 function ProjectBox(props) {
 	return (
 		<div className='l-projectbox'>
 			<figure>
-				<img src={props.project.imageUrl} alt='Imagem do projeto' title='Imagem do projeto' />
+				<img src={props.project.imageType == 'discord' ? DiscordIcon : ProgrammingIcon} alt='Imagem do projeto' title='Imagem do projeto' />
 				<figcaption><a target="_blank" href={props.project.projectUrl}>{props.project.name}</a></figcaption>
 			</figure>
 			<section>
