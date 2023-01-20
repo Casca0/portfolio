f7efff68296615de7e9c51217174b8e411af4447
Casca0 <lucashborzani@gmail.com>
2023-01-20 00:34:07 -0300

    Adição de botão para currículo

--- src/react/containers/Home/index.jsx
+++ src/react/containers/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.scss';
 
 import profilePic from '@assets/profilePic.webp';
+import curriculum from '@assets/curriculum.pdf';
 
 function Home() {
 	const todayDate = new Date();
@@ -22,6 +23,7 @@ function Home() {
 				<p>&#10024; {yearsOld - 1}y</p>
 				<p>&#x1F468;&#x1F3FC;&#x200D;&#x1F4BB; Desenvolvedor Front-end</p>
 				<p>&#127952; Atleta</p>
+				<a target="_blank" href={curriculum} download="CV-Lucas-Henrique-Borzani.pdf" className="l-home__curriculum" rel="noreferrer">Baixar Currículo</a>
 			</div>
 		</div>
 	);
